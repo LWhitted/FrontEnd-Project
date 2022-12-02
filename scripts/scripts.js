3eb2dafdc261a5539bf9f4a2f037b641b1979488
Victoria <victoriahope5683@gmail.com>
2022-12-01 21:00:38 -0600

    display image and create igredients list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,43 @@
-async function getCocktailRecipe(){
+async function getCocktailRecipe() {
     const response = await fetch("https://thecocktaildb.com/api/json/v1/1/random.php")
     const data = await response.json()
-    console.log(data.drinks[0])
+    //console.log(data.drinks[0])
     const drinkName = data.drinks[0].strDrink;
-    console.log(drinkName);
+    //console.log(drinkName);
     displayDrinkName(drinkName);
 }
 
 function displayDrinkName(drink){
-    console.log(drink)
+    //console.log(drink)
     const drinkHeader = document.getElementById("drink-header");
-    console.log(drinkHeader)
+    //console.log(drinkHeader)
     drinkHeader.innerText = `${drink}`;
 }
+
 
 async function getMealRecipe() {
     const response = await fetch("https://themealdb.com/api/json/v1/1/search.php?s=pasta")
     const data = await response.json()
     console.log(data.meals[0])
     const mealName = data.meals[0].strMeal
-    console.log(mealName)
     
     displayMealName(mealName);
-    
+    displayImage(data.meals[0].strMealThumb);
+
+    let mealIngredients = [];
+    let mealMeasurements = [];
+
+    const dataArray = Object.keys(data.meals[0]);
+    const ingredients = dataArray.filter(word => word.includes("Ingredient"));
+    const measurements = dataArray.filter(word => word.includes("Measure"));
+
+    for (i = 0; i <= ingredients.length; i++){
+        if (data.meals[0][ingredients[i]]){
+            mealIngredients.push(data.meals[0][ingredients[i]])
+            mealMeasurements.push(data.meals[0][measurements[i]])
+        }
+    }
+    displayIngredients(mealMeasurements, mealIngredients);
 }
 
 function displayMealName(mealName) {
@@ -30,5 +45,28 @@ function displayMealName(mealName) {
     mealHeader.innerHTML = `${mealName}`;
 }
 
+function displayImage(imgSrc) {
+    const imgTag = document.getElementById("meal-img")
+    imgTag.src = `${imgSrc}`;
+}
+
+function displayIngredients(measurements, ingredients) {
+    let bulletHtml = [];
+    for (i = 1; i < ingredients.length; i++){
+        bulletHtml.push(`${measurements[i]}` + ` ${ingredients[i].toLowerCase() }`)
+    }
+    console.log(bulletHtml)
+    // const list = document.getElementById("meal-list")
+    // bulletHtml.forEach((item) => {
+    //     console.log(item)
+    //     const li = document.createElement("li");
+    //     li.innerText = `${item}`;
+    //     list.appendChild;
+    // })
+}
+// Ive got the string that I want to put into each bullet point but 
+// the code I have commented out up there isnt appending to the dom 
+
 getMealRecipe();
 getCocktailRecipe();
+
