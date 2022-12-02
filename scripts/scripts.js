8f88ad73423bc18db0edc24632a9eb6a7d588663
kngsong1 <kristinagiannoutsos@gmail.com>
2022-12-01 21:29:09 -0600

    edits to script for cocktial recipes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,56 @@
+
+
 async function getCocktailRecipe(){
     const response = await fetch("https://thecocktaildb.com/api/json/v1/1/random.php")
     const data = await response.json()
     console.log(data.drinks[0])
-    const drinkName = data.drinks[0].strDrink;
-    console.log(drinkName);
+    let drinkName = data.drinks[0].strDrink;
+    
+    let drinkRecipe = drinkIngredients
+
+    
+    console.log("dogs are cool",drinkRecipe);
+
     displayDrinkName(drinkName);
+    displayDrinkRecipes(drinkIngredients)
+
+    // console.log("what's going on",Object.keys(data.drinks[0].strDrink))
+
+    const array0fIngredients = Object.keys(data.drinks[0])
+    const arrayIngredients = array0fIngredients.filter(word => word.includes("Ingredient"))
+    console.log("wow",arrayIngredients);
+
+    for (i=0; i <= arrayIngredients.length; i++){
+       if (data.drinks[0][arrayIngredients[i]]) {
+        drinkIngredients.push(data.drinks[0][arrayIngredients[i]])
+       }
+    }
+   console.log("final list",drinkIngredients)
+
+  
+   function displayDrinkRecipes(recipe) {
+    console.log(recipe)
+    // const drinkList = document.getElementById("drink-list")
+    // let innerHTML = [];
+
+    // for (i=0; i < drinkIngredients.length; i++) {
+    //     innerHTML.push(`<li class="list-group-item">${recipe}</li>`)  
+       
+    // }
+    //  console.log(innerHTML)
 }
+}
+ 
+
+
 
 function displayDrinkName(drink){
     console.log(drink)
     const drinkHeader = document.getElementById("drink-header");
-    console.log(drinkHeader)
+  
     drinkHeader.innerText = `${drink}`;
+
+  
 }
 
 async function getMealRecipe() {
